de9034413ecf1b9ae616427dc27fdd6d0e6cb4a2
KIDJourney <kingdeadfish@qq.com>
2016-03-24 12:06:02 +0800

    modify setup

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="translateit",
     version='0.0.1',
+    author="KIDJourney",
+    author_email="kingdeadfish@qq.com",
+    description="This is a simple console translator",
+    license="MIT",
+    keywords="hello world translate requests",
+    
     packages=find_packages('src'),
     package_dir={'': 'src'},
 
@@ -17,11 +23,6 @@ setup(
         'Programming Language :: Python :: 3',
     ],
 
-    author="KIDJourney",
-    author_email="kingdeadfish@qq.com",
-    description="This is a simple console translator",
-    license="MIT",
-    keywords="hello world translate requests",
 
     entry_points={
         'console_scripts': [
