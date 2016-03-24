0123e6046ea3bb7c804a05a3f59998595cadcbad
KIDJourney <kingdeadfish@qq.com>
2016-03-24 11:02:45 +0800

    update setup

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "translateit",
-    version = '0.0.1',
-    packages = find_packages(),
-    )
+    name="translateit",
+    version='0.0.1',
+    packages=find_packages(),
+    scripts=['translate.py'],
+
+    install_requires=['requests'],
+
+    author="KIDJourney",
+    author_email="kingdeadfish@qq.com",
+    description="This is a simple console translator",
+    license="MIT",
+    keywords="hello world translate requests",
+)
